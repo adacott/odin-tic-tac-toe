551fb08c8ba23ffb0b15f03a230ff96d29ef2dc6
Adam Cottrill <33773276+adacott@users.noreply.github.com>
2023-11-10 21:22:55 +0000

    Implemented basic score function, checkForTie functions

--- logic.js
+++ logic.js
@@ -9,53 +9,78 @@ const gameBoard = (function () {
         console.log(board);
     }
 
-    function placeMarker() {
-        this.innerHTML = "X";
+    function placeMarker(player) {
+        marker = player.marker
+        this.innerHTML = `${marker}`;
     }
 
     function checkForTie() {
-
+        if (board.every(item => item !== null)) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
     function checkForWin() {
 
     }
 
+    function clearBoard() {
+        board = [null, null, null, null, null, null, null, null, null];
+        console.log("Board reset");
+    }
 
-    return { displayBoard, placeMarker, checkForTie, checkForWin }
+    function iterateScore() {
 
+    }
+    return { displayBoard, placeMarker, checkForTie, checkForWin, clearBoard }
 }());
 
-function createPlayer(name, marker) {
+
+function createPlayer(name, marker, score) {
     function getPlayerInfo() {
         return `${name} = ${marker}`;
     }
-    return { getPlayerInfo, name, marker };
+    const updateScore = () => score++;
+    const displayScore = () => score;
+    return { getPlayerInfo, name, marker, updateScore, displayScore };
 }
 
 function gameFlow() {
     // Create player 1 object
     let name = prompt("Player 1, what is your name?: ", "Player 1");
     let marker = prompt(`${name}, what marker do you choose, X or O?: `, "X").toUpperCase();
-    const player1 = createPlayer(name, marker);
+    const player1 = createPlayer(name, marker, 0);
 
     // Create player 2 object
     name = prompt("Player 2, what is your name?: ", "Player 2");
     marker2 = (marker === "X") ? "O" : "X"; // use ternary operator to condense
-    const player2 = createPlayer(name, marker2);
+    const player2 = createPlayer(name, marker2, 0);
     console.log(`${player1.getPlayerInfo()} || ${player2.getPlayerInfo()}`);
 
     // Decide the current player (current player will always be "X")
     let currentPlayer = (player1.marker === "X") ? player1 : player2;
-    // let currentPlayer = player1;
     console.log(currentPlayer.name);
 
+    // Gameplay loop until win or tie
+    while (true) {
+        gameBoard.displayBoard();
 
+        // Place current player's marker based on where they clicked
+        gameBoard.placeMarker(currentPlayer);
 
+        // check for a winner
+        gameBoard.checkForWin();
 
+        // check for a tie
+        gameBoard.checkForTie();
 
+        // Iterate the score: 
+        gameBoard.iterateScore();
 
-
+    }
 
 
 
